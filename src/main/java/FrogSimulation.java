c56139757d6aaf4f7dee7ccfada4b9fef23cad3a
AlaBuck <Albuckingham@s.sfusd.edu>
2025-05-22 11:47:30 -0700

    Update FrogSimulation.java

--- src/main/java/FrogSimulation.java
+++ src/main/java/FrogSimulation.java
@@ -31,7 +31,7 @@ public class FrogSimulation
 	public double runSimulations(int num)
 	{ 
 		/* to be implemented in part (b) */ 
-		int succesfulruns = 0;
+		int successfulruns = 0;
 		for(int i=0; i<=num; i++){
 		     if(simulate()){
 			successfulruns++;
